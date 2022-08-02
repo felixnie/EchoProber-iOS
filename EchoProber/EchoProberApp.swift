018210549cfb82b871e599abfd14d956682f9b30
felixnie <hongtuo.nie@ntu.edu.sg>
2022-08-02 12:47:07 +0800

    Initial Commit

---
+++ EchoProber/EchoProberApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoProberApp.swift
+//  EchoProber
+//
+//  Created by Hongtuo on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct EchoProberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
